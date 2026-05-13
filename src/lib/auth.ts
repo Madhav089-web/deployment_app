7316a4cc4396b77bc49aa2c9709faff699ff7d0b
Madhav Maheshwari <madhavmaheshwari089@gmail.com>
2026-05-14 01:30:33 +0530

    Connect Neon database

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,14 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "./prisma";
 import bcrypt from "bcryptjs";
 
+
+
+
+
+
+
+
+
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
