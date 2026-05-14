2dbb9e2e86a6cafb4f8f81bdd0d9f16c4014dce9
Madhav Maheshwari <madhavmaheshwari089@gmail.com>
2026-05-14 08:21:55 +0530

    Connect Neon database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
 
+
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
